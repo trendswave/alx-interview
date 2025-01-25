6a1a686b437c7a6c39d8130d13bebe4f6675658b
trendswave <paulchinedumicheal@gmail.com>
2025-01-25 06:05:20 +0100

    File name changed

---
+++ 0x0A-primegame/main_0.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+island_perimeter = __import__('0-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+    
+    
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))  # Expected output: 12
